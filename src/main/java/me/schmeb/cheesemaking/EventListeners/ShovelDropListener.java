f7593390b2e49c2f97e10ca19f6cfa3846feb81c
BuildTools <unconfigured@null.spigotmc.org>
2023-06-05 13:23:13 +0800

    Test

---
+++ src/main/java/me/schmeb/cheesemaking/EventListeners/ShovelDropListener.java
@@ -0,0 +1,12 @@
+package me.schmeb.cheesemaking.EventListeners;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerDropItemEvent;
+
+public class ShovelDropListener implements Listener {
+    @EventHandler
+    public void onItemDrop(PlayerDropItemEvent event){
+
+    }
+}
