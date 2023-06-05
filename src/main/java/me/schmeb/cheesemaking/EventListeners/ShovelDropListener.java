754e69f7bb594813228c5e90702d5eb01f00cba2
BuildTools <unconfigured@null.spigotmc.org>
2023-06-05 13:26:57 +0800

    Test

--- src/main/java/me/schmeb/cheesemaking/EventListeners/ShovelDropListener.java
+++ src/main/java/me/schmeb/cheesemaking/EventListeners/ShovelDropListener.java
@@ -1,5 +1,6 @@
 package me.schmeb.cheesemaking.EventListeners;
 
+import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerDropItemEvent;
@@ -7,6 +8,6 @@ import org.bukkit.event.player.PlayerDropItemEvent;
 public class ShovelDropListener implements Listener {
     @EventHandler
     public void onItemDrop(PlayerDropItemEvent event){
-
+        if(event.getItemDrop().getItemStack().getType() != Material.GOLDEN_SHOVEL) return;
     }
 }
